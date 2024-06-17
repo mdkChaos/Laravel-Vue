7a45fb88ee2c8ccd008d754698d60ec6e2dc7584
Andrii Poludniev <mdkchaos1@gmail.com>
2024-06-17 19:09:47 +0200

    add data and methods

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,10 +1,39 @@
 <template>
-    <div>1111</div>
+    <div>
+        Post
+        <single-post-component>
+
+        </single-post-component>
+        <div>Name: {{ name }}</div>
+        <button @click="sayHello">Hello</button>
+        <button @click="sayGoodbye">Goodbye</button>
+    </div>
 </template>
 
 <script>
+import SinglePostComponent from './SinglePostComponent.vue';
 export default {
-    name: "PostComponent"
+    name: "PostComponent",
+
+    data(){
+        return {
+            name: "Andrii",
+            age: 41,
+        }
+    },
+
+    methods: {
+        sayHello() {
+            console.log('Hello');
+        },
+        sayGoodbye() {
+            console.log('Goodbye');
+        }
+    },
+
+    components: {
+        SinglePostComponent
+    }
 }
 </script>
 
